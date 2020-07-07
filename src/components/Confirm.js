712e8f26c5f17df805acd3bf695f0b461f1e5c5b
Soupoftheday201 <jaredbaker201@gmail.com>
2020-07-07 16:09:37 -0700

    Added confirm and success comp / updated user form

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -10,38 +10,40 @@ export class FormUserDetails extends Component {
     // Process Form //
     this.props.nextStep();
   };
+
+  back = (e) => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
   render() {
-    const { values, handleChange } = this.props;
+    const {
+      values: { firstName, lastName, email, occupation, city, bio },
+    } = this.props;
     return (
       <MuiThemeProvider>
         <React.Fragment>
-          <AppBar title="Enter User Details" />
-          <TextField
-            hintText="Enter Your First Name"
-            floatingLabelText="First Name"
-            onChange={handleChange("firstName")}
-            defaultValue={values.firstName}
-          />
-          <br />
-          <TextField
-            hintText="Enter Your Last Name"
-            floatingLabelText="Last Name"
-            onChange={handleChange("lastName")}
-            defaultValue={values.lastName}
-          />
-          <br />
-          <TextField
-            hintText="Enter Your Email"
-            floatingLabelText="Email"
-            onChange={handleChange("email")}
-            defaultValue={values.email}
-          />
+          <AppBar title="Confirm User Data" />
+          <List>
+            <ListItem primaryText="First Name" secondaryText={firstName} />
+            <ListItem primaryText="Last Name" secondaryText={lastName} />
+            <ListItem primaryText="Email" secondaryText={email} />
+            <ListItem primaryText="Occupation" secondaryText={occupation} />
+            <ListItem primaryText="City" secondaryText={city} />
+            <ListItem primaryText="Bio" secondaryText={bio} />
+          </List>
           <br />
           <RaisedButton
-            label="Continue"
+            label="Confirm & Continue"
             primary={true}
             style={styles.button}
             onClick={this.continue}
+          />
+          <RaisedButton
+            label="Back"
+            primary={false}
+            style={styles.button}
+            onClick={this.back}
           />
         </React.Fragment>
       </MuiThemeProvider>
